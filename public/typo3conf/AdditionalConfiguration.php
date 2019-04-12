50132279a2861c3fb33a92f755e71a913058c3a4
dmh <dmytro.hrynevych@gmail.com>
2019-04-12 15:58:05 +0300

    [TASK] simplify TYPO3 context

--- public/typo3conf/AdditionalConfiguration.php
+++ public/typo3conf/AdditionalConfiguration.php
@@ -2,8 +2,8 @@
 
 switch (\TYPO3\CMS\Core\Utility\GeneralUtility::getApplicationContext()) {
 
-    case 'Development/t3kit9': // =========================================================================
-        // SetEnv TYPO3_CONTEXT Development/t3kit9
+    case 'Development': // =========================================================================
+        // SetEnv TYPO3_CONTEXT Development
         // DB
         $GLOBALS['TYPO3_CONF_VARS']['DB']['Connections']['Default']['host'] = getenv('DB_CONTAINER_NAME');
         $GLOBALS['TYPO3_CONF_VARS']['DB']['Connections']['Default']['dbname'] = getenv('DB_NAME');
@@ -29,33 +29,8 @@ switch (\TYPO3\CMS\Core\Utility\GeneralUtility::getApplicationContext()) {
 
 
 
-    case 'Development/t3kit9-sqlite': // ==================================================================
-        // SetEnv TYPO3_CONTEXT Development/t3kit9-sqlite
-        // DB
-        $GLOBALS['TYPO3_CONF_VARS']['DB']['Connections']['Default']['driver'] = 'pdo_sqlite';
-        $GLOBALS['TYPO3_CONF_VARS']['DB']['Connections']['Default']['path'] = '/var/www/html/db/sqlite/t3kit9.sqlite';
-
-        // BE
-        $GLOBALS['TYPO3_CONF_VARS']['BE']['sessionTimeout'] = 28800;
-        $GLOBALS['TYPO3_CONF_VARS']['BE']['debug'] = true;
-
-        // FE
-        $GLOBALS['TYPO3_CONF_VARS']['FE']['debug'] = true;
-
-        // SYS
-        $GLOBALS['TYPO3_CONF_VARS']['SYS']['sitename'] = 'TYPO3 :: t3kit v9.0.0-alpha :: Dev mode *(SQLite)';
-        $GLOBALS['TYPO3_CONF_VARS']['SYS']['devIPmask'] = '*';
-        $GLOBALS['TYPO3_CONF_VARS']['SYS']['displayErrors'] = 1;
-        $GLOBALS['TYPO3_CONF_VARS']['SYS']['errorHandlerErrors'] = 30711; // Default = 30466
-        $GLOBALS['TYPO3_CONF_VARS']['SYS']['exceptionalErrors'] = 12290; // Default = 4096
-        $GLOBALS['TYPO3_CONF_VARS']['SYS']['belogErrorReporting'] = 30711; // Default = 30711
-
-    break; //__________________________________________________________________________________________________________________
-
-
-
-    case 'Development/t3kit9-test': // =============================================================================================
-        // SetEnv TYPO3_CONTEXT Development/t3kit9-test
+    case 'Testing': // =============================================================================================
+        // SetEnv TYPO3_CONTEXT Testing
         // DB
         $GLOBALS['TYPO3_CONF_VARS']['DB']['Connections']['Default']['host'] = getenv('DB_CONTAINER_NAME');
         $GLOBALS['TYPO3_CONF_VARS']['DB']['Connections']['Default']['dbname'] = getenv('DB_NAME');
@@ -78,7 +53,6 @@ switch (\TYPO3\CMS\Core\Utility\GeneralUtility::getApplicationContext()) {
         $GLOBALS['TYPO3_CONF_VARS']['SYS']['belogErrorReporting'] = 30711; // Default = 30711
 
     break; //__________________________________________________________________________________________________________________
-
 
 
 
