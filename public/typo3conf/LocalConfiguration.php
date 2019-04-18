b6aa87b5589426132d9cdfcc520289510e927075
dmh <dmytro.hrynevych@gmail.com>
2019-04-18 15:54:57 +0300

    update db, add pages

--- public/typo3conf/LocalConfiguration.php
+++ public/typo3conf/LocalConfiguration.php
@@ -24,9 +24,9 @@ return [
     ],
     'EXT' => [
         'extConf' => [
-            'backend' => 'a:6:{s:9:"loginLogo";s:0:"";s:19:"loginHighlightColor";s:0:"";s:20:"loginBackgroundImage";s:0:"";s:13:"loginFootnote";s:0:"";s:11:"backendLogo";s:0:"";s:14:"backendFavicon";s:0:"";}',
-            'extensionmanager' => 'a:2:{s:21:"automaticInstallation";s:1:"0";s:11:"offlineMode";s:1:"1";}',
-            'indexed_search' => 'a:20:{s:8:"pdftools";s:9:"/usr/bin/";s:8:"pdf_mode";s:2:"20";s:5:"unzip";s:9:"/usr/bin/";s:6:"catdoc";s:9:"/usr/bin/";s:6:"xlhtml";s:9:"/usr/bin/";s:7:"ppthtml";s:9:"/usr/bin/";s:5:"unrtf";s:9:"/usr/bin/";s:18:"trackIpInStatistic";s:1:"2";s:9:"debugMode";s:1:"0";s:18:"fullTextDataLength";s:1:"0";s:23:"disableFrontendIndexing";s:1:"0";s:21:"enableMetaphoneSearch";s:1:"1";s:6:"minAge";s:2:"24";s:6:"maxAge";s:1:"0";s:16:"maxExternalFiles";s:1:"5";s:26:"useCrawlerForExternalFiles";s:1:"0";s:11:"flagBitMask";s:3:"192";s:16:"ignoreExtensions";s:0:"";s:17:"indexExternalURLs";s:1:"0";s:16:"useMysqlFulltext";s:1:"0";}',
+            'backend' => 'a:6:{s:14:"backendFavicon";s:0:"";s:11:"backendLogo";s:0:"";s:20:"loginBackgroundImage";s:0:"";s:13:"loginFootnote";s:0:"";s:19:"loginHighlightColor";s:0:"";s:9:"loginLogo";s:0:"";}',
+            'extensionmanager' => 'a:2:{s:21:"automaticInstallation";s:1:"0";s:11:"offlineMode";s:1:"0";}',
+            'indexed_search' => 'a:20:{s:6:"catdoc";s:9:"/usr/bin/";s:9:"debugMode";s:1:"0";s:23:"disableFrontendIndexing";s:1:"0";s:21:"enableMetaphoneSearch";s:1:"1";s:11:"flagBitMask";s:3:"192";s:18:"fullTextDataLength";s:1:"0";s:16:"ignoreExtensions";s:0:"";s:17:"indexExternalURLs";s:1:"0";s:6:"maxAge";s:1:"0";s:16:"maxExternalFiles";s:1:"5";s:6:"minAge";s:2:"24";s:8:"pdf_mode";s:2:"20";s:8:"pdftools";s:9:"/usr/bin/";s:7:"ppthtml";s:9:"/usr/bin/";s:18:"trackIpInStatistic";s:1:"2";s:5:"unrtf";s:9:"/usr/bin/";s:5:"unzip";s:9:"/usr/bin/";s:26:"useCrawlerForExternalFiles";s:1:"0";s:16:"useMysqlFulltext";s:1:"0";s:6:"xlhtml";s:9:"/usr/bin/";}',
             'scheduler' => 'a:2:{s:11:"maxLifetime";s:4:"1440";s:15:"showSampleTasks";s:1:"1";}',
         ],
     ],
@@ -41,7 +41,7 @@ return [
         ],
         'extensionmanager' => [
             'automaticInstallation' => '0',
-            'offlineMode' => '1',
+            'offlineMode' => '0',
         ],
         'indexed_search' => [
             'catdoc' => '/usr/bin/',
@@ -101,6 +101,9 @@ return [
         'encryptionKey' => 'f41cf8ff1a13301597269971d43ecf5ec1e48989c5d0b959ab591772b5cb908e4967496b56e411877e510dab33cc02a2',
         'exceptionalErrors' => 12290,
         'features' => [
+            'TypoScript.strictSyntax' => true,
+            'redirects.hitCount' => false,
+            'simplifiedControllerActionDispatching' => false,
             'unifiedPageTranslationHandling' => true,
         ],
         'sitename' => 'New t3kit9 site',
