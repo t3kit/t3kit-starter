b0e4843c72424259cb237d58790c60a180a290e7
dmh <dmytro.hrynevych@gmail.com>
2019-08-21 14:24:12 +0300

    [TASK] update packDB script, add restoreDB step

--- .t3kit/db/packdb.sh
+++ .t3kit/db/packdb.sh
@@ -50,6 +50,20 @@ mysqldump --defaults-extra-file=$BASEDIR/mysql.cnf "$DB_NAME" > $BASEDIR/"$OUT_F
 echo "Merge be_users.sql table, must include 'admin' user with password 'admin1234'"
 cat $BASEDIR/be_users.sql >> "$BASEDIR/${OUT_FILE}"
 
+
+# drop and restore DB
+if [ "`mysql --defaults-extra-file=$BASEDIR/mysql.cnf -e 'show databases;' | grep ${DB_NAME}`" == "${DB_NAME}" ]; then
+
+    # Drop database
+    echo "DROP $DB_NAME DB"
+    mysql --defaults-extra-file=$BASEDIR/mysql.cnf -e "DROP DATABASE $DB_NAME;"
+
+    # Create new database
+    echo "CREATE NEW DATABASE $DB_NAME"
+    mysql --defaults-extra-file=$BASEDIR/mysql.cnf -e "CREATE DATABASE $DB_NAME"
+    mysql --defaults-extra-file=$BASEDIR/mysql.cnf $DB_NAME < $BASEDIR/t3kit9.sql
+fi
+
 # Remove temporary DB configuration
 rm $MYSQL_CONFIG
 
