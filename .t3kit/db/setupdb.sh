c4b2b79a41b100ca523619b45818398e3aecebe2
mabolek <mathias@lilio.com>
2019-10-24 14:12:47 +0200

    [TASK] Fixed indenting

--- .t3kit/db/setupdb.sh
+++ .t3kit/db/setupdb.sh
@@ -17,18 +17,18 @@ NUMBER_OF_ROWS=0
 
 # Check if the database exists
 if [[ -n "$(mysql -s -e \"SHOW DATABASES LIKE 'dbname';\")" ]]; then
-  NUMBER_OF_ROWS=$(mysql -s --skip-column-names -e "SELECT COUNT(DISTINCT table_name) FROM information_schema.columns WHERE table_schema = '$DB_NAME'");
+    NUMBER_OF_ROWS=$(mysql -s --skip-column-names -e "SELECT COUNT(DISTINCT table_name) FROM information_schema.columns WHERE table_schema = '$DB_NAME'");
 fi
 
 # Ask the user if it is OK to continue if there database is not empty
 if [ "$NUMBER_OF_ROWS" -gt 0 ]; then
-  read -rp "The database $DB_NAME is not empty. Would you like to continue? [Y/n]: " CHANGE_DB
+    read -rp "The database $DB_NAME is not empty. Would you like to continue? [Y/n]: " CHANGE_DB
 fi;
 
 if [ "$CHANGE_DB" = "Y" ]; then
-  mysql --defaults-extra-file="$BASEDIR/mysql.cnf" -e "CREATE DATABASE IF NOT EXISTS $DB_NAME"
-  mysql --defaults-extra-file="$BASEDIR/mysql.cnf" "$DB_NAME" < "$BASEDIR/t3kit10.sql"
-  echo "Done"
+    mysql --defaults-extra-file="$BASEDIR/mysql.cnf" -e "CREATE DATABASE IF NOT EXISTS $DB_NAME"
+    mysql --defaults-extra-file="$BASEDIR/mysql.cnf" "$DB_NAME" < "$BASEDIR/t3kit10.sql"
+    echo "Done"
 fi
 
 # Remove temporary DB configuration
--- .t3kit/ddev/setupddev.sh
+++ .t3kit/ddev/setupddev.sh
@@ -2,21 +2,21 @@
 
 # Check if DDEV is installed and asks user to configure for ddev
 if [ -n "$(command -v ddev)" ]; then
-  read -rp "Would you like to set up and configure DDEV? [Y/n]: " SETUP_DDEV
+    read -rp "Would you like to set up and configure DDEV? [Y/n]: " SETUP_DDEV
 
-  if [ "$SETUP_DDEV" != "Y" ]; then
-    echo "OK. Won't install DDEV. You can do this later by running"
-    echo "    .t3kit/ddev/setupddev.sh"
-    exit 0
-  fi
+    if [ "$SETUP_DDEV" != "Y" ]; then
+        echo "OK. Won't install DDEV. You can do this later by running"
+        echo "    .t3kit/ddev/setupddev.sh"
+        exit 0
+    fi
 
-  BASEDIR=$(dirname "$0")
+    BASEDIR=$(dirname "$0")
 
-  cp -r "$BASEDIR/dot-ddev-directory" ../../.ddev
+    cp -r "$BASEDIR/dot-ddev-directory" ../../.ddev
 
-  echo "The .ddev directory has been created."
+    echo "The .ddev directory has been created."
 
-  cd "$BASEDIR/../.." && ddev config --docroot=public --project-type=typo3 --php-version=7.2
+    cd "$BASEDIR/../.." && ddev config --docroot=public --project-type=typo3 --php-version=7.2
 fi
 
 
