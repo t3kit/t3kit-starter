5e3337926210608812fbdf23eecd148e23099afd
dmh <dmytro.hrynevych@gmail.com>
2019-10-30 10:11:30 +0200

    [BUGFIX] fix issue with connecting to local MySQL server for setupup.sh

--- .t3kit/db/setupdb.sh
+++ .t3kit/db/setupdb.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-set -eu pipefail
+set -eu
+set -o pipefail
 
 # ####################################
 readonly BASEDIR=$(dirname "${BASH_SOURCE[0]}")
@@ -17,17 +18,18 @@ NUMBER_OF_ROWS=0
 
 # Check if the database exists
 if [[ -n $(mysql --defaults-extra-file="$BASEDIR"/mysql.cnf -s -e "SHOW DATABASES LIKE '$DB_NAME';") ]]; then
-    NUMBER_OF_ROWS=$(mysql -s --skip-column-names -e "SELECT COUNT(DISTINCT table_name) FROM information_schema.columns WHERE table_schema = '$DB_NAME'");
+    NUMBER_OF_ROWS=$(mysql --defaults-extra-file="$BASEDIR"/mysql.cnf -s --skip-column-names -e "SELECT COUNT(DISTINCT table_name) FROM information_schema.columns WHERE table_schema = '$DB_NAME'");
 fi
 
 # Ask the user if it is OK to continue if there database is not empty
-if [ "$NUMBER_OF_ROWS" -gt 0 ]; then
+if [[ "$NUMBER_OF_ROWS" -gt 0 ]]; then
     read -rp "The database $DB_NAME is not empty. Would you like to continue? [Y/n]: " CHANGE_DB
 fi
 
-if [ "$CHANGE_DB" = "Y" ]; then
+if [[ "$CHANGE_DB" = "Y" ]]; then
     mysql --defaults-extra-file="$BASEDIR"/mysql.cnf -e "CREATE DATABASE IF NOT EXISTS $DB_NAME"
     mysql --defaults-extra-file="$BASEDIR"/mysql.cnf "$DB_NAME" < "$BASEDIR/t3kit10.sql"
+    mysql --defaults-extra-file="$BASEDIR"/mysql.cnf -e "SHOW DATABASES"
     echo "Done"
 fi
 
