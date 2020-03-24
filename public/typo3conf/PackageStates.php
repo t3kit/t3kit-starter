0a37898f7e61936a8615bc12f0b7ae81879f3f7b
Mats Svensson <mats@pixelant.se>
2020-03-24 11:45:10 +0100

    [FEATURE] add and enable cms-dashboard core extension (#21)

--- public/typo3conf/PackageStates.php
+++ public/typo3conf/PackageStates.php
@@ -62,6 +62,9 @@ return [
         'beuser' => [
             'packagePath' => 'typo3/sysext/beuser/',
         ],
+        'dashboard' => [
+            'packagePath' => 'typo3/sysext/dashboard/',
+        ],
         'extensionmanager' => [
             'packagePath' => 'typo3/sysext/extensionmanager/',
         ],
