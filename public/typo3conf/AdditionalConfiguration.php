37c2526344cadd8b7f1a3fe46c72a7a4b241fc18
dmh <dmytro.hrynevych@gmail.com>
2020-06-04 17:45:24 +0300

    [TASK] remove compressionLevel option, should be added separately

--- public/typo3conf/AdditionalConfiguration.php
+++ public/typo3conf/AdditionalConfiguration.php
@@ -52,11 +52,9 @@ switch (\TYPO3\CMS\Core\Core\Environment::getContext()) {
         // BE
         $GLOBALS['TYPO3_CONF_VARS']['BE']['sessionTimeout'] = 3600;
         $GLOBALS['TYPO3_CONF_VARS']['BE']['debug'] = false;
-        // $GLOBALS['TYPO3_CONF_VARS']['BE']['compressionLevel'] = 5;
 
         // FE
         $GLOBALS['TYPO3_CONF_VARS']['FE']['debug'] = false;
-        $GLOBALS['TYPO3_CONF_VARS']['FE']['compressionLevel'] = 5;
 
         // SYS
         $GLOBALS['TYPO3_CONF_VARS']['SYS']['sitename'] = 'TYPO3 :: t3kit10 :: Production *(local)';
@@ -93,11 +91,9 @@ switch (\TYPO3\CMS\Core\Core\Environment::getContext()) {
         // BE
         $GLOBALS['TYPO3_CONF_VARS']['BE']['sessionTimeout'] = 3600;
         $GLOBALS['TYPO3_CONF_VARS']['BE']['debug'] = false;
-        // $GLOBALS['TYPO3_CONF_VARS']['BE']['compressionLevel'] = 5;
 
         // FE
         $GLOBALS['TYPO3_CONF_VARS']['FE']['debug'] = false;
-        $GLOBALS['TYPO3_CONF_VARS']['FE']['compressionLevel'] = 5;
 
         // SYS
         $GLOBALS['TYPO3_CONF_VARS']['SYS']['sitename'] = 'TYPO3 :: t3kit10 :: Production';
