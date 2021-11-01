6c133b85430008e4b609a2d8c069edc879cb52e4
dmh <dmh@users.noreply.github.com>
2021-11-01 17:45:52 +0200

    test

--- .localconf/db/setupdb.sh
+++ .localconf/db/setupdb.sh
@@ -11,6 +11,8 @@ BASEDIR=$(dirname "${BASH_SOURCE[0]}")
 # ####################################
 docker compose exec -T web /var/www/html/"$BASEDIR"/cmd/createdb.sh
 docker compose exec -T web /var/www/html/"$BASEDIR"/cmd/importdb.sh
+docker compose exec -T -u www-data web /var/www/html/vendor/bin/typo3 extension:setup
+docker compose exec -T -u www-data web /var/www/html/vendor/bin/typo3 cache:flush
 # ####################################
 
 echo -e "\nDone\n"
