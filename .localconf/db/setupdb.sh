0f9ea8a006ae162597d11d38906c4c5aa2ea7512
dmh <dmh@users.noreply.github.com>
2021-11-01 16:04:56 +0200

    [TASK] update theme_newcustomproject, declare strict types in ext_localconf.php file

--- .localconf/db/setupdb.sh
+++ .localconf/db/setupdb.sh
@@ -11,7 +11,6 @@ BASEDIR=$(dirname "${BASH_SOURCE[0]}")
 # ####################################
 docker compose exec -T web /var/www/html/"$BASEDIR"/cmd/createdb.sh
 docker compose exec -T web /var/www/html/"$BASEDIR"/cmd/importdb.sh
-docker compose exec -T -u www-data web /var/www/html/vendor/bin/typo3 cache:flush
 # ####################################
 
 echo -e "\nDone\n"
