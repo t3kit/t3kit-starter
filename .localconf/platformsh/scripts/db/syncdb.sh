c5921390ea33f9854300ecccab2319cfdd9b5430
Mattias Nilsson <tollepjaer@gmail.com>
2022-09-29 16:19:53 +0200

    [BUGFIX] Add missing typo3-console scripts for syncdb command (#92)

--- .localconf/platformsh/scripts/db/syncdb.sh
+++ .localconf/platformsh/scripts/db/syncdb.sh
@@ -16,6 +16,8 @@ BASEDIR=$(dirname "${BASH_SOURCE[0]}")
 docker compose exec -T web /var/www/html/"$BASEDIR"/cmd/dropdb.sh
 docker compose exec -T web /var/www/html/"$BASEDIR"/cmd/createdb.sh
 docker compose exec -T web /var/www/html/"$BASEDIR"/cmd/importdb.sh
+docker compose exec -T -u www-data web /var/www/html/vendor/helhum/typo3-console/typo3cms install:fixfolderstructure
+docker compose exec -T -u www-data web /var/www/html/vendor/helhum/typo3-console/typo3cms database:updateschema
 docker compose exec -T -u www-data web /var/www/html/vendor/bin/typo3 extension:setup
 docker compose exec -T -u www-data web /var/www/html/vendor/bin/typo3 language:update
 docker compose exec -T -u www-data web /var/www/html/vendor/bin/typo3 cache:flush
